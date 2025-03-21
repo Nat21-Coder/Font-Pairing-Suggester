195af6f70a227d7898187f15e03a9d3c60574f7e
Nat21-Coder <natnaelkifle08@gmail.com>
2025-03-21 20:19:17 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { Toaster } from "@/components/ui/toaster"
-
+import { Suspense } from "react";
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
